9a1a762f17006df1c3d696554224716e97d7f91c
Perdente <58706003+Perdente@users.noreply.github.com>
2021-05-01 02:16:37 +0600

    Create 55. Jump Game.cpp

---
+++ 55. Jump Game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int can_reach=0,n=nums.size();
+        for(int i=0;i<=can_reach;++i){
+            if(i==n-1)return true;
+            can_reach=max(can_reach,i+nums[i]);
+        }
+        return false;
+    }
+};
