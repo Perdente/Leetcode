7780a69ff5dade2050fc823b3b44884e8ee818d1
Perdente <58706003+Perdente@users.noreply.github.com>
2021-05-01 02:11:54 +0600

    Create 231. Power of Two.cpp

---
+++ 231. Power of Two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+      if(n<=0)return false;
+      return !(n&(n-1));
+    }
+};
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+      if(n<0)return false;
+      int cnt=0;
+      while(n)cnt++,n&=(n-1);
+      return cnt==1;
+    }
+};
